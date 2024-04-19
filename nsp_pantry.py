2e53726ec8327c377ca5152eb9d4cc6dc107f17c
kuroazai <pyromwansa@gmail.com>
2024-04-19 18:35:04 +0100

    - clean up - works cross-platform windows/linux - resolved issue when trying to read file from windows

--- nsp_pantry.py
+++ nsp_pantry.py
@@ -1,34 +1,31 @@
-nspterminology = None
+import random
+import json
+import os
+import requests
+from typing import Union
 
-def wget(url, output):
-    import subprocess
-    res = subprocess.run(['wget', '-q', url, '-O', output], stdout=subprocess.PIPE).stdout.decode('utf-8')
-    print(res)
 
-# nsp_parse( prompt )
-# Input: dict, list, str
-# Parse strings for terminology keys and replace them with random terms
-def nsp_parse(prompt):
+def download_pantry(url: str, output: str):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception(f'Failed to download {url}')
+    print(f'Downloading {url} to {output}')
+    with open(output, 'wb') as f:
+        f.write(response.content)
+        f.close()
 
-    import random, os, json
 
-    global nspterminology
-	
-    new_prompt = ''
+def nsp_parse(prompt: Union[list, dict, str], nspterminology: dict = None) -> dict:
     new_prompts = []
     new_dict = {}
-    ptype = type(prompt)
-	
-    if not os.path.exists('./nsp_pantry.json'):
-        wget('https://raw.githubusercontent.com/WASasquatch/noodle-soup-prompts/main/nsp_pantry.json', './nsp_pantry.json')
 
-    if nspterminology is None:
-        with open('./nsp_pantry.json', 'r') as f:
-    	    nspterminology = json.loads(f.read())
+    if not nspterminology:
+        with open('./nsp_pantry.json', 'r', encoding='cp932', errors='ignore') as f:
+            nspterminology = json.loads(f.read())
 
-    if ptype == dict:
+    if isinstance(prompt, dict):
         for pstep, pvalue in prompt.items():
-            if type(pvalue) == list:
+            if isinstance(pvalue, list):
                 for prompt in pvalue:
                     new_prompt = prompt
                     for term in nspterminology:
@@ -40,7 +37,7 @@ def nsp_parse(prompt):
                 new_dict[pstep] = new_prompts
                 new_prompts = []
         return new_dict
-    elif ptype == list:
+    elif isinstance(prompt, list):
         for pstr in prompt:
             new_prompt = pstr
             for term in nspterminology:
@@ -49,9 +46,8 @@ def nsp_parse(prompt):
                 for i in range(tcount):
                     new_prompt = new_prompt.replace(tkey, random.choice(nspterminology[term]), 1)
             new_prompts.append(new_prompt)
-            new_prompt = None
         return new_prompts
-    elif ptype == str:
+    elif isinstance(prompt, str):
         new_prompt = prompt
         for term in nspterminology:
             tkey = f'_{term}_'
@@ -59,5 +55,8 @@ def nsp_parse(prompt):
             for i in range(tcount):
                 new_prompt = new_prompt.replace(tkey, random.choice(nspterminology[term]), 1)
         return new_prompt
-    else:
-        return 
+
+
+if not os.path.exists('./nsp_pantry.json'):
+    download_pantry('https://raw.githubusercontent.com/WASasquatch/noodle-soup-prompts/main/nsp_pantry.json',
+                    './nsp_pantry.json')
